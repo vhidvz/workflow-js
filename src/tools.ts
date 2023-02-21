63da02169543d890b484b8ad32708ab3cd496a08
Vahid V <vhid.vz@gmail.com>
2023-02-21 22:49:06 +0330

    update

--- src/tools.ts
+++ src/tools.ts
@@ -3,6 +3,14 @@ import { Activity, EventActivity, GatewayActivity, Sequence, TaskActivity } from
 import { BPMNActivity, BPMNEvent, BPMNGateway, BPMNProcess, BPMNTask } from './type';
 import { IdentityOptions } from './common';
 
+/**
+ * It takes a BPMNProcess and an optional options object, and returns an Activity
+ *
+ * @param {BPMNProcess} process - The BPMNProcess object that contains the activity.
+ * @param [options] - { key: string; activity: BPMNActivity }
+ *
+ * @returns A new Activity object
+ */
 export const getActivity = (process: BPMNProcess, options?: { key: string; activity: BPMNActivity }) => {
   if (!options) return new Activity(process);
 
@@ -21,6 +29,17 @@ export const getActivity = (process: BPMNProcess, options?: { key: string; activ
   return new Activity(process, activity, key);
 };
 
+/**
+ * It takes a list of outgoing sequences and an optional identity, and returns a list of target
+ * references
+ *
+ * @param {Sequence[]} outgoing - Sequence[] - the outgoing sequence array
+ * @param {IdentityOptions} [identity] - IdentityOptions
+ *
+ * @returns The function takes in a sequence and an identity. If the identity is defined, it will
+ * filter the sequence by the id or name of the identity. If the identity is not defined, it will
+ * return the sequence.
+ */
 export const takeOutgoing = (outgoing: Sequence[], identity?: IdentityOptions) => {
   if (identity) {
     if (identity && 'id' in identity)
--- src/utils.ts
+++ src/utils.ts
@@ -4,8 +4,20 @@ import { IdentityOptions } from './common';
 import { parseString } from 'xml2js';
 import * as fs from 'fs';
 
+/**
+ * ReadFile takes a string and returns a string
+ *
+ * @param {string} path - string - The path to the file you want to read.
+ */
 export const readFile = (path: string): string => fs.readFileSync(path, 'utf8');
 
+/**
+ * It takes an XML string and returns a BPMN schema
+ *
+ * @param {string} xml - string - the XML string to parse
+ *
+ * @returns A BPMNSchema object
+ */
 export const parse = (xml: string): BPMNSchema => {
   let parse;
   parseString(xml, { async: false }, (err, result) => {
@@ -18,6 +30,14 @@ export const parse = (xml: string): BPMNSchema => {
   return parse;
 };
 
+/**
+ * It takes a BPMNProcess and an IdentityOptions and returns an object with a key and an activity
+ *
+ * @param {BPMNProcess} process - The BPMNProcess object
+ * @param {IdentityOptions} identity - IdentityOptions
+ *
+ * @returns An object with two properties: key and activity.
+ */
 export const getBPMNActivity = (process: BPMNProcess, identity: IdentityOptions) => {
   for (const [key, activities] of Object.entries(process)) {
     if (typeof activities === 'object' && Array.isArray(activities)) {
@@ -31,6 +51,14 @@ export const getBPMNActivity = (process: BPMNProcess, identity: IdentityOptions)
   }
 };
 
+/**
+ * It takes a BPMN definition and an identity object, and returns the process that matches the identity
+ *
+ * @param {BPMNDefinition} definition - The BPMN definition object.
+ * @param {IdentityOptions} identity - IdentityOptions
+ *
+ * @returns A BPMNProcess
+ */
 export const getBPMNProcess = (definition: BPMNDefinition, identity: IdentityOptions) => {
   const processes = definition['bpmn:process'];
   const collaborations = definition['bpmn:collaboration'];
@@ -49,4 +77,7 @@ export const getBPMNProcess = (definition: BPMNDefinition, identity: IdentityOpt
   return processes.find((process) => process.$.id === identity.id);
 };
 
+/**
+ * It returns a random string of 36 characters
+ */
 export const uid = () => Math.round(Date.now() * Math.random() * 10).toString(36);
