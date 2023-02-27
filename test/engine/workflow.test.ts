05481189ccdec784fe24a3d3a812cf9212605533
Vahid V <vhid.vz@gmail.com>
2023-02-27 16:04:21 +0330

    update

--- test/engine/workflow.test.ts
+++ test/engine/workflow.test.ts
@@ -1,19 +1,56 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { BPMNSchema, parse, readFile, getBPMNProcess, BPMNProcess, Activity } from '../../src';
+import {
+  BPMNSchema,
+  parse,
+  readFile,
+  getBPMNProcess,
+  BPMNProcess,
+  Process,
+  Node,
+  Act,
+  EventActivity,
+  WorkflowJS,
+  Status,
+  Data,
+  Value,
+} from '../../src';
+
+@Process({ name: 'Simple Workflow' })
+class SimpleWorkflow {
+  @Node({ name: 'Start' })
+  public start(@Act() activity: EventActivity, @Data() data: any, @Value() value: any) {
+    activity.takeOutgoing();
+
+    return data && value;
+  }
+}
 
 describe('test workflow engine class', () => {
   let xml: string;
   let schema: BPMNSchema;
-  let activity: Activity | undefined;
 
   let process: BPMNProcess | undefined;
 
   it('should return process', () => {
-    xml = readFile('./example/supplying-pizza.bpmn');
+    xml = readFile('./example/simple-workflow.bpmn');
 
     schema = parse(xml);
-    process = getBPMNProcess(schema['bpmn:definitions'], { id: 'Process_166h0sl' });
+    process = getBPMNProcess(schema['bpmn:definitions'], { id: 'Process_1igpwhg' });
 
     expect(process).toBeDefined();
+  });
+
+  it('should return workflow', () => {
+    const workflow = WorkflowJS.build();
+
+    expect(workflow).toBeDefined();
+
+    const { context } = workflow.execute({
+      factory: () => new SimpleWorkflow(),
+      schema: parse(xml)['bpmn:definitions'],
+    });
+
+    expect(context.status).toBe(Status.Terminated);
   });
 });
